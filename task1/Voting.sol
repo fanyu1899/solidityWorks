89e674f73b23196bc1b83d1c919a3b2c8508c7e5
fanyu1899 <fanyu1899@gmail.com>
2025-08-14 17:14:22 +0800

    task  1.1 vote

---
+++ task1/Voting.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.26;
+
+contract Voting{
+    mapping (string name => uint voteNums)  public votemap;
+    mapping(string => bool) private keyExists;
+
+    string[] public keys;
+
+    function vote(string calldata name) public{
+        votemap[name] += 1;
+        if(!keyExists[name]){
+            keys.push(name);
+            keyExists[name] = true;
+        }
+    }
+
+    function getVote(string calldata name) public view returns(uint){
+       return votemap[name];
+    }
+
+    function resetVotes() public {
+        for (uint i = 0; i < keys.length; i++) {
+            delete votemap[keys[i]];
+        }
+        
+        delete keys;
+    }
+
+}
